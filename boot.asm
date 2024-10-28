48045565c6a5e9e9ddb4d5b4859281682d1da396
Adam Ellouze <elzadam11@gmail.com>
2024-10-28 15:34:59 +0100

    Create boot.asm

---
+++ boot.asm
@@ -0,0 +1,18 @@
+bits 16
+org 0x7c00
+
+mov si, 0
+
+print:
+  mov ah, 0x0e
+  mov al, [hello + si]
+  int 0x10
+  add si, 1
+  cmp [hello + si], 0
+  jne print
+  
+hello:
+  db "Hello X86", 0
+
+times 510 -($ - $$) db 0
+dw 0xAA55:
